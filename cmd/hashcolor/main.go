be71ccc42ead76a3f2fbd2646f3786fac4e58903
Serge Tkatchouk <sp1j3t@gmail.com>
2021-08-05 01:05:06 +0800

    Add support for iTerm on MacOS

--- cmd/hashcolor/main.go
+++ cmd/hashcolor/main.go
@@ -11,11 +11,16 @@ import (
 
 func hex(c color.Color) string {
 	rgba := color.RGBAModel.Convert(c).(color.RGBA)
-	return fmt.Sprintf("#%.2x%.2x%.2x", rgba.R, rgba.G, rgba.B)
+	return fmt.Sprintf("%.2x%.2x%.2x", rgba.R, rgba.G, rgba.B)
 }
 
 func main() {
 	c := utils.New(strings.Join(os.Args[1:], " "))
 	t, s := utils.Tint(c), utils.Shade(c)
-	fmt.Printf("\033]10;%s\007\033]11;%s\007", hex(t), hex(s))
+	term := os.Getenv("TERM_PROGRAM")
+	if (term == "iTerm.app") {
+		fmt.Printf("\033]1337;SetColors=fg=rgb:%s\033\\\033]1337;SetColors=bg=rgb:%s\033\\", hex(t), hex(s))
+	} else {
+		fmt.Printf("\033]10;#%s\007\033]11;#%s\007", hex(t), hex(s))
+	}
 }
